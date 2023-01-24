f2ac80128b07ffb65c3d18eac41b9d6ac9718618
Sébastien Allamand <sallaman@amazon.com>
2023-01-24 18:53:40 +0000

    allow logs of exs exec

--- examples/core-infra/main.tf
+++ examples/core-infra/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
 
 locals {
   name   = var.core_stack_name
@@ -101,9 +102,44 @@ module "vpc" {
   tags = local.tags
 }
 
+resource "aws_kms_key" "this" {
+  description             = "KMS key for ecs exec logging"
+  deletion_window_in_days = 10
+  
+   policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "key-default-1",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.aws_region}.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    }  
+  ]
+}
+EOF
+}
+
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/ecs/${local.name}"
   retention_in_days = 7
+  kms_key_id = aws_kms_key.this.arn
 
   tags = local.tags
 }
